9f1e922ba1d042ef087ad50b1024f0c75dbd78b4
Destin McMurry <destinmcmurry@gmail.com>
2018-04-03 13:47:00 -0400

    a little cuter

--- app/components/Homerooms.js
+++ app/components/Homerooms.js
@@ -12,7 +12,9 @@ export default (props) => {
             return (
               <li key={homeroom.id} className='homeroom-block'>
                 <img src={homeroom.teacherImg} />
-                <h1>{homeroom.teacher}</h1>
+                <h1>{homeroom.teacher}'s Class</h1>
+                <h4>Room {homeroom.roomNumber}</h4>
+                <p>{homeroom.description}</p>
               </li>
             )
           })
