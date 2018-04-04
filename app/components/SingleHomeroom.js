21476a5f5a0d90f23479271ab40e424b534fd6fe
Destin McMurry <destinmcmurry@gmail.com>
2018-04-04 13:05:13 -0400

    single homeroom rendering

--- app/components/SingleHomeroom.js
+++ app/components/SingleHomeroom.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { NavLink } from 'react-router-dom';
 import store from '../store';
 import AddStudent from './AddStudent';
 
 const SingleHomeroom = (props) => {
 
   return (
-    <div> 
-      <h1>all students will go here eventually</h1>
+    <div>
+      <ul> 
+        {
+          props.students.map(student => {
+            if (student.homeroomId === Number(props.match.params.id)) {
+              return (
+                <li key={student.id} className='student-block'>
+                <NavLink to={`/students/${student.id}`}>
+                  <img src={student.studentImg} />
+                  <h1>{student.name}</h1>
+                  <h4>{student.email}</h4>
+                  <p>gpa: {student.gpa}</p>
+                </NavLink>
+              </li>
+              )
+            }
+          })
+        }
+      </ul>
       <br/>
       <div id='footer-form'>
         <h3>New Student</h3>
